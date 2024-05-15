a38bced57c692ebf3874fb62209080db43b8d015
Zrinka Alilović <alzrinka1@gmail.com>
2024-05-15 11:21:02 +0200

    Move files

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import HeroTitle from "../../components/body/HeroTitle";
+
+function About() {
+    return (
+    <>
+        <Header />
+        <HeroTitle title="We are a tight-knit, experienced crew and we make end-to-end digital products. ↘"/>
+        <Footer />
+    </>
+    )
+}
+
+export default About;
