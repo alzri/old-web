ce215e5a380c0f5ad2d5a5faa370e7af97787a55
Zrinka Alilović <alzrinka1@gmail.com>
2024-05-21 15:24:47 +0200

    Creating project card array

--- src/components/body/ProjectCardLayout.jsx
+++ src/components/body/ProjectCardLayout.jsx
@@ -1,72 +1,29 @@
-import ProjectCard from "./ProjectCard";
-import FoodtruxBanner from "../../assets/img/foodtrux-small.jpg?react";
-import FoodtruxIcon from "../../assets/img/foodtrux.svg?react";
-import Dobartekbanner from "../../assets/img/dobartek-small.jpg?react";
-import DobartekIcon from "../../assets/img/dobar-tek-logo.svg?react";
-import McKinseybanner from '../../assets/img/mckinsey-large.jpg?react';
-import McKinseyIcon from "../../assets/img/mckinsey.svg?react";
-import HajdukBanner from "../../assets/img/Homepage-HajdukBanner.png?react";
-import HajdukIcon from "../../assets/img/Hajduk.svg?reatc";
-import NutricionistBanner from "../../assets/img/nutricionist-small.jpg?react";
-import NutricionistIcon from "../../assets/img/nutricionist.svg?react";
-import BitcoinBanner from "../../assets/img/bitcoin-store-large.jpg?react";
-import BitcoinIcon from "../../assets/img/Bitcoin-store.svg?react";
+import React, { useEffect, useState } from 'react';
+import ProjectCard from './ProjectCard';
+import projectsData from '../../api/projects.json';
 
-function ProjectCardLayout() {
+const ProjectCardLayout = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        setProjects(projectsData);
+    }, []);
+
     return (
-        <>
-           <div className="projects-grid">
-           <ProjectCard
-                projectLink="/foodtrux"
-                projectBanner={FoodtruxBanner}
-                projectIcon={FoodtruxIcon}
-                projectTitle="Foodtrux Apps"
-                projectDesc="Discover food trucks near you"
-                gridClass="grid-6"
-            />
-            <ProjectCard
-                projectLink="/dobartek"
-                projectBanner={Dobartekbanner}
-                projectIcon={DobartekIcon}
-                projectTitle="Dobartek Apps"
-                projectDesc="Food delivery service"
-                gridClass="grid-6"
-            />
-             <ProjectCard
-                projectLink="/mckinsey"
-                projectBanner={McKinseybanner}
-                projectIcon={McKinseyIcon}
-                projectTitle="McKinsey&Co"
-                projectDesc="Expense and travel management app"
-                gridClass="grid-12"
-            />
-             <ProjectCard
-                projectLink="/hajduk"
-                projectBanner={HajdukBanner}
-                projectIcon={HajdukIcon}
-                projectTitle="HNK Hajduk Split"
-                projectDesc="The most significant foodball club in Croatia"
-                gridClass="grid-6"
-            />
-             <ProjectCard
-                projectLink="/nutricionist"
-                projectBanner={NutricionistBanner}
-                projectIcon={NutricionistIcon}
-                projectTitle="Nutricionist"
-                projectDesc="Digitalisation of nutritionist practice using Mobile and Web Apps"
-                gridClass="grid-6"
-            />
-             <ProjectCard
-                projectLink="/bitcoin-store"
-                projectBanner={BitcoinBanner}
-                projectIcon={BitcoinIcon}
-                projectTitle="Bitcoin Store"
-                projectDesc="Buy and sell Bitcoin and other crypto"
-                gridClass="grid-12"
-            />
-           </div>
-        </>
+        <div className='projects-grid'>
+            {projects.map((project, index) => (
+                <ProjectCard
+                    key={index}
+                    projectLink={project.projectLink}
+                    projectIcon={project.projectIcon}
+                    projectBanner={project.projectBanner}
+                    projectTitle={project.projectTitle}
+                    projectDesc={project.projectDesc}
+                    gridClass={project.gridClass}
+                />
+            ))}
+        </div>
     );
-}
+};
 
 export default ProjectCardLayout;
