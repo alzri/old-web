c0a79de8a5d7e1c667baa4c9bc1e8a6ed4b7b372
Zrinka Alilović <alzrinka1@gmail.com>
2024-05-21 14:23:59 +0200

    Using useLocation

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/adp-logo.svg?react';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
-  const [activeLink, setActiveLink] = useState("home");
-  
-  const handleLinkClick = (link) => {
-    setActiveLink(link);
-  }
+  const location = useLocation();
+  const currentPath = location.pathname;
 
   return (
     <header>
@@ -19,21 +14,11 @@ function Header() {
         </span>
         <nav>
           <ul className="header--links">
-            <li className={`nav__link ${activeLink === "home" ? "active" : ""}`}>
-              <Link
-                to="/"
-                onClick={() => handleLinkClick("home")}
-              >
-                Home
-              </Link>
+            <li className={`nav__link ${currentPath === "/" ? "active" : ""}`}>
+              <Link to="/">Home</Link>
             </li>
-            <li className={`nav__link ${activeLink === "about" ? "active" : ""}`}>
-              <Link
-                to="/about"
-                onClick={() => handleLinkClick("about")}
-              >
-                About
-              </Link>
+            <li className={`nav__link ${currentPath === "/about" ? "active" : ""}`}>
+              <Link to="/about">About</Link>
             </li>
           </ul>
         </nav>
